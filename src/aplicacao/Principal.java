3210f9ab200d20536238361f94615f9b68eb74e4
George Gonçalves <georgeggmiranda@gmail.com>
2024-11-28 23:36:56 -0300

    criado os objetos Vendedor e Departamento

--- src/aplicacao/Principal.java
+++ src/aplicacao/Principal.java
@@ -1,12 +1,20 @@
 package aplicacao;
 
-import bancoDados.BancoDados;
+import java.util.Date;
+
+import modelo.entidades.Departamento;
+import modelo.entidades.Vendedor;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		BancoDados.conectar();
+		Departamento dep = new Departamento(1, "Eletronicos");
+		System.out.println(dep);
+		
+		Vendedor ven = new Vendedor(
+				111, "Maria Aparecida", "maria@hotmail.com", new Date(), 4000.0, dep);
+		System.out.println(ven);
 	}
 
 }
